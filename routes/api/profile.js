71dfa4105b724cce34b7c5fc9e6538a7f31af48f
lukecolpo <lwcolpo@gmail.com>
2019-11-27 21:36:26 -0500

    Delete Profile & User

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -138,4 +138,24 @@ router.get('/user/:user_id', async (req,res) => {
 	}
 });
 
+//  @route 	DELETE  api/profile
+//	@desc 	Delete profile, user & posts
+//	@access private
+router.delete('/', auth, async (req,res) => {
+	try {
+		// @todo - remove users posts
+		// remove profile
+		await Profile.findOneAndRemove({user: req.user.id });
+		// remove user
+		await User.findOneAndRemove({_id: req.user.id });
+		res.json({msg: 'User removed'});
+	} catch (err) {
+		console.error(err.message);
+		if(err.kind == 'ObjectId'){
+			return res.status(400).json({msg: 'Profile Not Found'});
+		}
+		res.status(500).send('Server Error');
+	}
+});
+
 module.exports = router;
